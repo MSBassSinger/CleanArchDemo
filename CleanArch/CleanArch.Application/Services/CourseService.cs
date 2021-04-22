2ff49a0e91c62ddf9d59f32bf585b85033463b63
Jeff Jones <Jeff.Jones@softvision.com>
2021-04-22 13:12:29 -0400

    Commit services and repositories

---
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,32 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+	public class CourseService : ICourseService
+	{
+		private ICourseRepository _CourseRepository;
+
+		public CourseService(ICourseRepository courseRepository)
+		{
+
+			_CourseRepository = courseRepository;
+
+		}
+
+
+		public CourseViewModel GetCourses()
+		{
+			return new CourseViewModel()
+			{
+				Courses = _CourseRepository.GetCourses()
+			};
+	}
+	}
+}
